90cd05754c1af1a7f4c977f56402274e9ef05474
karenfuji <fuji-karen-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-18 17:28:20 +0900

    検索、テキスト

--- source/src/main/java/dto/texts.java
+++ source/src/main/java/dto/texts.java
@@ -2,95 +2,51 @@ package dto;
 
 import java.util.Date;
 
-//テキストのデータを格納
 public class texts {
 	private int id;
-	private String text_name;
-	private int subject_id;
-	private int personality_id;
+	private String textName;
+	private int subjectId;
+	private int personalityId;
 	private int pages;
-	private String notes;		//備考
-	private Date created_at;
-	private Date updated_at;
-	
-	public texts(int id, String text_name, int subject_id, int personality_id, int pages, String notes, Date created_at,
-			Date updated_at) {
+	private String note;
+	private Date createdAt;
+	private Date updatedAt;
+
+	public texts() {}
+
+	public texts(int id, String textName, int subjectId, int personalityId, int pages, String note, Date createdAt, Date updatedAt) {
 		this.id = id;
-		this.text_name = text_name;
-		this.subject_id = subject_id;
-		this.personality_id = personality_id;
+		this.textName = textName;
+		this.subjectId = subjectId;
+		this.personalityId = personalityId;
 		this.pages = pages;
-		this.notes = notes;
-		this.created_at = created_at;
-		this.updated_at = updated_at;
-	}
-	
-	public texts() {
-		this(0,"",0,0,0,"",null,null);
+		this.note = note;
+		this.createdAt = createdAt;
+		this.updatedAt = updatedAt;
 	}
 
-	public int getId() {
-		return id;
-	}
+	// Getter / Setter
+	public int getId() { return id; }
+	public void setId(int id) { this.id = id; }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+	public String getTextName() { return textName; }
+	public void setTextName(String textName) { this.textName = textName; }
 
-	public String getText_name() {
-		return text_name;
-	}
+	public int getSubjectId() { return subjectId; }
+	public void setSubjectId(int subjectId) { this.subjectId = subjectId; }
 
-	public void setText_name(String text_name) {
-		this.text_name = text_name;
-	}
+	public int getPersonalityId() { return personalityId; }
+	public void setPersonalityId(int personalityId) { this.personalityId = personalityId; }
 
-	public int getSubject_id() {
-		return subject_id;
-	}
+	public int getPages() { return pages; }
+	public void setPages(int pages) { this.pages = pages; }
 
-	public void setSubject_id(int subject_id) {
-		this.subject_id = subject_id;
-	}
+	public String getNote() { return note; }
+	public void setNote(String note) { this.note = note; }
 
-	public int getPersonality_id() {
-		return personality_id;
-	}
+	public Date getCreatedAt() { return createdAt; }
+	public void setCreatedAt(Date createdAt) { this.createdAt = createdAt; }
 
-	public void setPersonality_id(int personality_id) {
-		this.personality_id = personality_id;
-	}
-
-	public int getPages() {
-		return pages;
-	}
-
-	public void setPages(int pages) {
-		this.pages = pages;
-	}
-
-	public String getNotes() {
-		return notes;
-	}
-
-	public void setNotes(String notes) {
-		this.notes = notes;
-	}
-
-	public Date getCreated_at() {
-		return created_at;
-	}
-
-	public void setCreated_at(Date created_at) {
-		this.created_at = created_at;
-	}
-
-	public Date getUpdated_at() {
-		return updated_at;
-	}
-
-	public void setUpdated_at(Date updated_at) {
-		this.updated_at = updated_at;
-	}
-	
+	public Date getUpdatedAt() { return updatedAt; }
+	public void setUpdatedAt(Date updatedAt) { this.updatedAt = updatedAt; }
 }
