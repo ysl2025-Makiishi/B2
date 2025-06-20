a77347897e64b413892923b65313d5269973a0cb
plusdojo2025-B2-suzuki <suzuki-yuma-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-20 17:42:47 +0900

    宿題提案

--- source/src/main/java/servlet/HomeworkServlet.java
+++ source/src/main/java/servlet/HomeworkServlet.java
@@ -2,40 +2,67 @@ package servlet;
 
 import java.io.IOException;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class MenuServlet
- */
+import dao.HomeworkDAO;
+
 @WebServlet("/HomeworkServlet")
 public class HomeworkServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-	 *      response)
-	 */
-	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		// もしもログインしていなかったらログインサーブレットにリダイレクトする
-//		HttpSession session = request.getSession();
-//		if (session.getAttribute("id") == null) {
-//			response.sendRedirect("/webapp/LoginServlet");
-//			return;
-//		}
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
 
-//		BcDAO dao = new BcDAO();
-//		int hitCount = dao.countAll();
-//		request.setAttribute("hitCount", hitCount - 1);
+        String studentIdStr = request.getParameter("studentId");
+        String subjectIdStr = request.getParameter("subjectId");
 
-		// メニューページにフォワードする
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/Homework.jsp");
-		dispatcher.forward(request, response);
-	}
+        if (studentIdStr == null || subjectIdStr == null) {
+            response.getWriter().println("studentId と subjectId をURLに指定してください。");
+            return;
+        }
+
+        int studentId = Integer.parseInt(studentIdStr);
+        int subjectId = Integer.parseInt(subjectIdStr);
+
+        request.setAttribute("studentId", studentId);
+        request.setAttribute("subjectId", subjectId);
+
+        request.getRequestDispatcher("/WEB-INF/jsp/Homework.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        request.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html; charset=UTF-8");
+
+        try {
+            int studentId = Integer.parseInt(request.getParameter("studentId"));
+            int subjectId = Integer.parseInt(request.getParameter("subjectId"));
+            int pagesPerDay = Integer.parseInt(request.getParameter("pagesPerDay"));
+            String action = request.getParameter("action"); // insert or update
+
+            HomeworkDAO dao = new HomeworkDAO();
+
+            if ("update".equals(action)) {
+                dao.updateHomework(studentId, subjectId, pagesPerDay);
+            } else {
+                dao.insertHomework(studentId, subjectId, pagesPerDay);
+            }
+
+            response.sendRedirect("SubjectResultServlet?studentId=" + studentId + "&subjectId=" + subjectId);
+
+        } catch (Exception e) {
+            response.getWriter().println("<h3>エラーが発生しました。</h3>");
+            response.getWriter().println("<pre>");
+            e.printStackTrace(response.getWriter());
+            response.getWriter().println("</pre>");
+        }
+    }
 }
